a171da65e1a180bf013ba60f0c727513cde7b003
surajbahugunalc <surajb@linecraft.ai>
2024-09-11 11:15:35 +0530

    adb

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import io from 'socket.io-client';
 import './App.css';
 
 // const socket = io('http://localhost:4000');
-const socket = io('https://kishmishbck.vercel.app/');
+const socket = io('https://kishmishbck.vercel.app', {
+  transports: ['websocket'] // Force WebSocket and disable polling
+});
 
 const INTERESTS = ['Music', 'Movies', 'Sports', 'Gaming', 'Travel', 'Food', 'Technology', 'Art'];
 
